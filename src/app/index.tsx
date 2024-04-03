19b966b46a2fce20acad88d04bb69b0c54228dbb
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-03 23:13:52 +0300

    Add QueryProvider to shared layer & use it in app/index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Home } from '../pages/home'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryProvider } from '~/shared/ui/QueryProvider';
+import { Home } from '../pages/home';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <QueryProvider>
+      <Home />
+    </QueryProvider>
   </React.StrictMode>,
-)
+);
