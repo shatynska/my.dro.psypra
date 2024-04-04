a5b0407fb0ce44c6364916198effe3ee0e7bd90a
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-04 11:19:08 +0300

    Update eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,77 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
+    '@feature-sliced',
+    '@feature-sliced/eslint-config/rules/import-order/experimental',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'tailwind.config.ts',
+    'postcss.config.js',
+    'vite.config.ts',
+    'vitest.config.ts',
+    'kubb.config.js',
+    'notes',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'Latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  plugins: ['react', 'react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'react/require-default-props': [
+      'warn',
+      {
+        functions: 'defaultArguments',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.config*',
+          '**/*.test.*',
+          '**/*.spec.*',
+          '**/mock*/**',
+          '**/vitest*',
+        ],
+      },
+    ],
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
 };
