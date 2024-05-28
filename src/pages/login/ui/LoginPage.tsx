7595df3db5cbf45355400f88bca2ec43ce897ede
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-28 16:23:39 +0300

    Insert LoginForm into LoginPage

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,7 +1,11 @@
+import { LoginForm } from '~/features/authorization/login';
+
 export function LoginPage() {
   return (
     <section className="w-full">
-      <div className="flex h-screen items-center justify-center">Логін</div>
+      <div className="flex h-screen items-center justify-center">
+        <LoginForm />
+      </div>
     </section>
   );
 }
