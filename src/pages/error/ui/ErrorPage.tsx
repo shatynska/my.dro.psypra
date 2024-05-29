7e5697a842ec930413399eace6e13425cfc591e7
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-29 10:50:44 +0300

    Refine navigation & styles on error page

--- src/pages/error/ui/ErrorPage.tsx
+++ src/pages/error/ui/ErrorPage.tsx
@@ -1,14 +1,26 @@
-import { Link, useRouteError } from 'react-router-dom';
+import { Link, Navigate, useRouteError } from 'react-router-dom';
+
+import { useAppSelector } from '~/shared/model/use-app-selector';
+
+import { userModel } from '~/entities/user';
 
 export function ErrorPage() {
   const error = useRouteError() as Error;
 
+  const authorizedUser = useAppSelector(userModel.selectUser);
+
+  if (!authorizedUser) {
+    return <Navigate to="/login" replace />;
+  }
+
   // TODO Refine ErrorPage
   return (
-    <>
+    <div className="m-auto flex h-screen flex-col items-center justify-center gap-4">
       <h1>Помилка</h1>
       <p>{error.message}</p>
-      <Link to="/">На головну</Link>
-    </>
+      <p>
+        <Link to={`/${authorizedUser.userName}/main`}>Перейти на головну</Link>
+      </p>
+    </div>
   );
 }
