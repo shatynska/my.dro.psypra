6bbb229c379131b0a802173b2f5389af28a1b834
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-31 23:57:44 +0300

    Use SidebarWidget in ProtectedLayout

--- src/app/layouts/ProtectedLayout.tsx
+++ src/app/layouts/ProtectedLayout.tsx
@@ -7,6 +7,7 @@ import { Skeleton } from '~/shared/ui/skeleton';
 import { userModel } from '~/entities/user';
 
 import { HeaderWidget } from '~/widgets/header';
+import { SidebarWidget } from '~/widgets/sidebar';
 
 export function ProtectedLayout() {
   const authorizedUser = useAppSelector(userModel.selectUser);
@@ -19,6 +20,7 @@ export function ProtectedLayout() {
     <>
       <HeaderWidget />
       <div className="flex">
+        <SidebarWidget />
         <main className="w-full bg-muted p-8">
           <Suspense fallback={<Skeleton />}>
             <Outlet />
